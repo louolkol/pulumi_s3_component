7142cee72b9c6e916cef01af832842e9cde969ba
Peter Chong <louolkol@gmail.com>
2023-02-09 17:28:53 +0800

    working version

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+// JS version
+// const s3folder = require("./s3folder.js");
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// // Create an instance of the S3Folder component
+// let folder = new s3folder.S3Folder("pulumi-static-site", "./www");
+
+// // Export `folder` output properties as stack outputs
+// exports.bucketName = folder.bucketName;
+// exports.websiteUrl = folder.websiteUrl;
+
+// TS version
+// require the component file
+const s3folder = require("./component/s3.ts");
+
+// Create an instance of the S3Folder component
+let folder = new s3folder.S3Folder("pulumi-static-site", "./www");
+
+// Export `folder` output properties as stack outputs
+exports.bucketName = folder.bucketName;
+exports.websiteUrl = folder.websiteUrl;
